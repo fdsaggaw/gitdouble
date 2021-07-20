df6b111ab450d2be6211136f7de91f21bd57c42d
suwliang <suwliang_3@163.com>
2021-07-20 14:42:47 +0800

    second

--- src/com/su/demo/Hello.java
+++ src/com/su/demo/Hello.java
@@ -3,5 +3,6 @@ package com.su.demo;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("ssssss");
+        System.out.println("sssss2222222222s");
     }
 }
